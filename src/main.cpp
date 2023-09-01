5576f4c4670e02c2e8b85857e682ec7db2a5a88a
mr.toor <42692077+Zer0-bit@users.noreply.github.com>
2023-09-01 11:07:47 +0100

    change to swap

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
                   , area->y1
                   , area->x2 - area->x1 + 1
                   , area->y2 - area->y1 + 1
-                  , ( lgfx::rgb565_t* )&color_p->full );
+                  , ( lgfx::swap565_t* )&color_p->full );
   lv_disp_flush_ready(disp);
 }
 
